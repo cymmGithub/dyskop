43a366bb94c5fc55feaad0629070c93d88e34344
Przemek Swiercz <przemek.swiercz@mindyra.com>
2024-03-08 00:21:30 +0100

    Upgrade react; Add contact form (front-end);

--- app/kontakt/page.tsx
+++ app/kontakt/page.tsx
@@ -1,8 +1,118 @@
+/*
+  This example requires some changes to your config:
 
-export default function Contact() {
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+
+export default function Example() {
 	return (
-		<div className='md:container mx-auto text-center'>
-			<h1>Kontakt </h1>
+		<div className='isolate bg-white px-6 pt-5 pb=10 lg:px-8'>
+			<div
+				className='absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]'
+				aria-hidden='true'
+			></div>
+			<div className='mx-auto max-w-2xl text-center'>
+				<h2 className='text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl'>
+					Napisz do nas
+				</h2>
+				<p className='mt-2 text-lg leading-8 text-gray-600'>
+					Zazwyczaj odpisujemy ciągu 24 h :)
+				</p>
+			</div>
+			<form
+				action='mailto:contact@yourdomain.com'
+				method='POST'
+				name='EmailForm'
+				className='mx-auto mt-7 mb-3 max-w-xl'
+			>
+				<div className='grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2'>
+					<div>
+						<label
+							htmlFor='first-name'
+							className='block text-sm font-semibold leading-6 text-gray-900'
+						>
+							Imię
+						</label>
+						<div className='mt-2.5'>
+							<input
+								type='text'
+								name='first-name'
+								id='first-name'
+								autoComplete='given-name'
+								className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+							/>
+						</div>
+					</div>
+					<div>
+						<label
+							htmlFor='last-name'
+							className='block text-sm font-semibold leading-6 text-gray-900'
+						>
+							Nazwisko
+						</label>
+						<div className='mt-2.5'>
+							<input
+								type='text'
+								name='last-name'
+								id='last-name'
+								autoComplete='family-name'
+								className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+							/>
+						</div>
+					</div>
+					<div className='sm:col-span-2'>
+						<label
+							htmlFor='email'
+							className='block text-sm font-semibold leading-6 text-gray-900'
+						>
+							Email
+						</label>
+						<div className='mt-2.5'>
+							<input
+								type='email'
+								name='email'
+								id='email'
+								autoComplete='email'
+								className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+							/>
+						</div>
+					</div>
+					<div className='sm:col-span-2'>
+						<label
+							htmlFor='message'
+							className='block text-sm font-semibold leading-6 text-gray-900'
+						>
+							Wiadomość
+						</label>
+						<div className='mt-2.5'>
+							<textarea
+								name='message'
+								id='message'
+								rows={4}
+								className='block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6'
+								defaultValue={''}
+							/>
+						</div>
+					</div>
+				</div>
+				<div className='mt-10'>
+					<button
+						type='submit'
+						className='block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
+					>
+						Wyślij
+					</button>
+				</div>
+			</form>
 		</div>
 	);
 }
