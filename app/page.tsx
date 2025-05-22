fce474bc9e4c1a9aecaf7d7c7afb28171d721974
Przemek Swiercz <przemek.swiercz@mindyra.com>
2025-05-22 22:58:51 +0200

    Enhance homepage layout by adjusting card component styles, improving responsiveness, and refining animation effects

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Link from 'next/link';
 import { Button } from '@/components/ui/button';
-import { FaPhone } from 'react-icons/fa6';
 import Card from '@/components/ui/card';
 
 export default function Home() {
@@ -9,29 +8,32 @@ export default function Home() {
 			<div className='absolute inset-0 sketchy-bg mt-12 md:mt-20'></div>
 			<div className='container mx-auto h-full px-5 sm:px-10 py-1 md:p-8 flex flex-col relative z-10 pt-20 md:pt-28'>
 				<div
-					className='flex flex-col max-w-5xl mx-auto w-full gap-4 md:gap-4 animate-slideUp opacity-0 pt-3 md:pt-8'
+					className='flex flex-col max-w-5xl mx-auto w-full gap-2 md:gap-4 animate-slideUp opacity-0 pt-3 md:pt-8'
 					style={{ animationDelay: '0.5s', animationFillMode: 'forwards' }}
 				>
 					<div
 						className='text-2xl sm:text-2xl md:text-4xl flex flex-col items-center font-bold
-						relative mb-1 md:mb-0'
+						relative '
 					>
-						<h1 className='relative px-4 py-2 z-10 me-5 text-black'>
+						<h1 className='relative px-4 py-2 z-10 me-0 sm:me-5 text-black'>
 							USŁUGI MINIKOPARKĄ
 						</h1>
 					</div>
 					<div className='text-black flex flex-row gap-8 sm:gap-12 md:gap-20 font-bold text-sm sm:text-base md:text-xl items-center justify-center italic ms-4'>
 						<div className='relative'>
-							<h3 className='relative z-10'>PRACE ZIEMNE</h3>
-							<span className='absolute left-4 bottom-0 w-[110%] h-3 bg-amber-400/40 -skew-x-12 -z-10 transform -translate-x-[5%]'></span>
+							<h3 className='relative z-10 inline transform -skew-x-12 bg-[linear-gradient(rgba(245,158,11,0.4),rgba(245,158,11,0.4))] bg-[length:100%_0.75rem] bg-bottom bg-no-repeat [box-decoration:clone] [-webkit-box-decoration:clone] px-2 text-transparent'>
+								<span className="inline transform skew-x-12 text-black">PRACE ZIEMNE</span>
+							</h3>
 						</div>
 						<div className='relative'>
-							<h3 className='relative z-10'>ODWIERTY WIERTNICĄ</h3>
-							<span className='absolute left-4 bottom-0 w-[110%] h-3 bg-amber-400/40 -skew-x-12 -z-10 transform -translate-x-[5%]'></span>
+							<h3 className='relative z-10 inline transform -skew-x-12 bg-[linear-gradient(rgba(245,158,11,0.4),rgba(245,158,11,0.4))] bg-[length:100%_0.75rem] bg-bottom bg-no-repeat [box-decoration:clone] [-webkit-box-decoration:clone] px-2 text-transparent'>
+								<span className="inline transform skew-x-12 text-black">ODWIERTY WIERTNICĄ</span>
+							</h3>
 						</div>
 						<div className='relative'>
-							<h3 className='relative z-10'>WSTAWIANIE SŁUPKÓW</h3>
-							<span className='absolute left-4 bottom-0 w-[110%] h-3 bg-amber-400/40 -skew-x-12 -z-10 transform -translate-x-[5%]'></span>
+							<h3 className='relative z-10 inline transform -skew-x-12 bg-[linear-gradient(rgba(245,158,11,0.4),rgba(245,158,11,0.4))] bg-[length:100%_0.75rem] bg-bottom bg-no-repeat [box-decoration:clone] [-webkit-box-decoration:clone] px-2 text-transparent'>
+								<span className="inline transform skew-x-12 text-black">WSTAWIANIE SŁUPKÓW</span>
+							</h3>
 						</div>
 					</div>
 					<p
@@ -44,7 +46,7 @@ export default function Home() {
 								max-w-2xl
 								mx-auto
 								leading-relaxed md:leading-normal
-								mb-2 md:mb-12'
+								md:mb-12'
 						style={{ animationDelay: '1s', animationFillMode: 'forwards' }}
 					>
 						Dzięki niewielkim gabarytom i dużej mocy maszyny, jesteśmy w stanie
--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+"use client";
+import React, { useState } from 'react';
 import Image from 'next/image';
 
 const cards = [
@@ -32,23 +33,24 @@ const cards = [
 ];
 
 const Card = () => {
+  const [activeMobileCard, setActiveMobileCard] = useState<string | null>(null);
+
   return (
-    <div className="flex justify-center items-center relative me-12">
+    <div className="flex justify-center items-center relative sm:me-12 mt-6 md:mt-0">
       <div className="flex justify-center items-center relative group/fan">
         {cards.map((card, idx) => (
           <div
             key={card.label}
             data-text={card.label}
-            className={`group/item relative w-52 h-52 bg-white/90 border border-white/10 shadow-lg cursor-pointer flex justify-center items-center transition-all duration-500 rounded-lg mx-[-50px] backdrop-blur-md rotate-[var(--r)] group-hover/fan:rotate-0 group-hover/fan:mx-[10px] ${card.label === 'Wiertnica' ? 'z-10' : 'z-0'}`}
+            onClick={() => {
+              setActiveMobileCard(prev => prev === card.label ? null : card.label);
+            }}
+            className={`group/item relative w-44 h-44 sm:w-52 sm:h-52 bg-white/90 border border-white/10 shadow-lg cursor-pointer flex justify-center items-center transition-all duration-500 rounded-lg backdrop-blur-sm -mx-16 ${activeMobileCard === card.label ? 'z-20 scale-105 -translate-y-2 rotate-0' : `${card.label === 'Wiertnica' ? 'z-10' : 'z-0'} rotate-[var(--r)]`}  sm:group-hover/fan:rotate-0 sm:group-hover/fan:mx-[10px] ${card.label === 'Wiertnica' ? 'sm:z-10' : 'sm:z-0'}`}
             style={{
-              // @ts-ignore
               '--r': `${card.r}deg`,
             } as React.CSSProperties}
           >
             {card.icon}
-            <span className="absolute bottom-0 w-full h-10 bg-white/5 flex justify-center items-center text-white text-base font-semibold pointer-events-none select-none">
-              {card.label}
-            </span>
           </div>
         ))}
       </div>
