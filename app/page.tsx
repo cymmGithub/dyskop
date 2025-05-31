f305e44c9c25a224568a2bbe5fcb8af7db9212c5
Przemek Swiercz <przemek.swiercz@mindyra.com>
2025-05-31 21:37:03 +0200

    Center things a bit on the main page

--- app/page.tsx
+++ app/page.tsx
@@ -14,13 +14,13 @@ export default function Home() {
 				>
 					<div
 						className='text-2xl sm:text-2xl md:text-4xl flex flex-col items-center font-bold
-						relative '
+						relative md:me-10'
 					>
-						<h1 className='relative px-4 py-2 z-10 me-0 sm:me-5 text-black'>
+						<h1 className='relative px-4 py-2 z-10 text-black'>
 							USŁUGI MINIKOPARKĄ
 						</h1>
 					</div>
-					<div className='text-black flex flex-row gap-8 sm:gap-12 md:gap-20 font-bold text-sm sm:text-base md:text-xl items-center justify-center italic ms-4'>
+					<div className='text-black flex flex-row gap-8 sm:gap-12 md:gap-20 font-bold text-sm sm:text-base md:text-xl items-center justify-center italic ms-4 text-center'>
 						<Link href='/zakres-uslug' className='relative'>
 							<h3 className='relative z-10 inline transform -skew-x-12 bg-[linear-gradient(rgba(245,158,11,0.4),rgba(245,158,11,0.4))] bg-[length:100%_0.75rem] bg-bottom bg-no-repeat [box-decoration:clone] [-webkit-box-decoration:clone] px-2 text-transparent hover:bg-[length:100%_1rem] transition-all duration-300 cursor-pointer'>
 								<span className="inline transform skew-x-12 text-black">PRACE ZIEMNE</span>
