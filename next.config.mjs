74b7126effdbc9cb4b4803254c6ca077f8dbc9b1
Przemek Swiercz <przemek.swiercz@mindyra.com>
2026-02-22 21:45:45 +0100

    Add backward compatibility for 'zakres-uslug' url

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const nextConfig = {
 			},
 		],
 	},
+	async redirects() {
+		return [
+			{
+				source: "/zakres-uslug",
+				destination: "/uslugi-minikoparka-lubelskie",
+				permanent: true,
+			},
+		];
+	},
 };
 
 export default nextConfig;
